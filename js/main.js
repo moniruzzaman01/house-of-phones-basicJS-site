fbf5d5a8ed9c02c87a15b693f571a9dcfb2360a6
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-03-01 11:56:00 +0600

    details added withour sensors

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ const callApi = () => {
     if (searchText == '') {
         searchText = null;
     }
-    // console.log('hello', searchText)
-    // searchText = 'samsung';
+    searchText = 'iphone';
     // spinner
     const spinner = document.getElementById('spinner')
     spinner.classList.remove('opacity-0')
@@ -19,7 +18,8 @@ const callApi = () => {
     // clean the search box
     searchTextField.value = '';
 }
-// callApi()
+callApi()
+
 // get data from api
 const getPhones = (data) => {
     const resultDiv = document.getElementById('results')
@@ -34,6 +34,7 @@ const getPhones = (data) => {
         spinner.classList.add('opacity-0')
     } else {
         const phones = data.data;
+        let counter = 0;
         phones.forEach(phone => {
             const {
                 brand,
@@ -51,11 +52,16 @@ const getPhones = (data) => {
                         <h5 class="card-title">${phone_name}</h5>
                         <p class="card-text"><small>Brand: ${brand}</small></p>
                     </div>
-                    <button onclick="showDetails('${slug}')" class="btn btn-sm btn-warning w-50 m-2">More...</button>
+                    <button onclick="getDetails('${slug}')" class="btn btn-sm btn-warning w-50 m-2">More...</button>
                 </div>
             `;
-            div.innerHTML = template;
-            resultDiv.appendChild(div);
+            console.log(counter)
+            counter++;
+            if (counter <= 20) {
+                div.innerHTML = template;
+                resultDiv.appendChild(div);
+                console.log('hello')
+            }
             // spinner hide
             spinner.classList.remove('opacity-100')
             spinner.classList.add('opacity-0')
@@ -63,10 +69,63 @@ const getPhones = (data) => {
         });
     }
 }
-const showDetails = (id) => {
+const getDetails = (id) => {
     // console.log(id);
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.slug))
+        .then(data => showDetails(data.data))
+}
+const showDetails = (data) => {
+    let {
+        name,
+        releaseDate,
+        image,
+        mainFeatures
+    } = data;
+    console.log(mainFeatures)
+    const {
+        chipSet,
+        displaySize,
+        memory,
+        sensors,
+        storage
+    } = mainFeatures;
+    if (releaseDate == '') {
+        releaseDate = 'date not found!'
+    }
+    const detailsDiv = document.getElementById('details')
+
+    const template = `
+    <div style="max-height: 100vh; width: 60%;" class="card mb-3 h-100 mx-auto">
+            <img src="${image}" style="width: 60%; overflow: hidden;" class="card-img-top mx-auto"
+                alt="images/img.jpg">
+            <div class="card-body">
+                <h2 class="card-title">${name}</h2>
+                <p class="card-text"><small>${releaseDate}</small></p>
+            </div>
+            <div class="container row row-cols-md-2">
+                <div class="col">
+                    <p>Chipset: ${chipSet}</p>
+                    <p>DisplaySize: ${displaySize}</p>
+                    <p>Memory: ${memory}</p>
+                    <p>Storage: ${storage}</p>
+                </div>
+                <div class="col">
+                        <p>
+                            Sensors:
+                            <ul>
+                                <li>1</li>
+                                <li>1</li>
+                                <li>1</li>
+                                <li>1</li>
+                                <li>1</li>
+                                <li>1</li>
+                            </ul>
+                        </p>
+                    </div>
+            </div>
+        </div>
+    `;
+    detailsDiv.innerHTML = template;
 }
